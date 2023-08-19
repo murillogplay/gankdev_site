620940e48d12063e73ee403b9a40e7c406df8b7b
Murillo Medeiros de Melo Nascimento <Murillo.gplay@gmail.com>
2023-08-19 15:19:48 -0300

    Base dos components do site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router'; 
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  { path: 'home', loadComponent: () => import('./components/pages/home/home.component').then(c => c.HomeComponent)},
+  { path: 'news', loadComponent: () => import('./components/pages/news/news.component').then(c => c.NewsComponent)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule] 
 })
 export class AppRoutingModule { }
