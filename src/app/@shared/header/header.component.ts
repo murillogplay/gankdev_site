cd7331b0f508634961ef7873780664828f533b95
Murillo Medeiros de Melo Nascimento <Murillo.gplay@gmail.com>
2023-08-22 09:28:57 -0300

    Correções no menu e pagina de contatos

--- src/app/@shared/header/header.component.ts
+++ src/app/@shared/header/header.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Route, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { LoginButtonComponent } from './components/login-button/login-button.component';
 import { UserHeaderComponent } from './components/user-header/user-header.component';
 import { AuthService } from '@auth0/auth0-angular';
@@ -12,19 +12,46 @@ import { AuthService } from '@auth0/auth0-angular';
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+
+  showMenu = true
+  
+  isMobile = window.innerWidth <= 1024;
+
   constructor(
     private router: Router,
-    public authService: AuthService){
-  } 
+    public authService: AuthService){}  
+    
+  @HostListener('window:resize', ['$event'])
+  onResize(event: { target: { outerWidth: number } }): void {
+    this.isMobile = event.target.outerWidth <= 1024;  
+    if(this.isMobile) this.closeMenu();
+    else this.openMenu();
 
+  }
+  
   goHome(){
     this.router.navigate(['/']); 
+    this.closeMenu();
   }
 
   goNews(){
-    this.router.navigate(['/news']); 
+    this.router.navigate(['/news']);
+    this.closeMenu();
   } 
   goContact(){
-    this.router.navigate(['/contact']); 
-  } 
+    this.router.navigate(['/contact']);  
+    this.closeMenu();
+  }  
+
+  closeMenu(){
+    if(this.isMobile) this.showMenu = false;
+  }
+
+  openMenu(){
+     this.showMenu = true; 
+  }
+
+  toogleMenu(){  
+      this.showMenu=!this.showMenu; 
+  }
 }
